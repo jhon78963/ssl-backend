987e6945c8a5f53fc22d9888cf594caf19f3caf6
Jhon Livias <jhonlivias3@gmail.com>
2024-08-24 11:57:42 -0500

    fixing

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SSLaiss Api Docs</title>
+</head>
+<body>
+   <h1> Here api document by Swagger </h1>
+</body>
+</html>

74e60279856cb11c57d442e1bce728f81d62d326
Jhon Livias <jhonlivias3@gmail.com>
2025-07-06 21:43:50 -0500

    mariadb docker

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTION');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Auth-Token');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTION');
+// header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Auth-Token');
 
 foreach (glob(app_path('*/Routes/public_api.php')) as $routeFile) {
     require $routeFile;
